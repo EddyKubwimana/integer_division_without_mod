91dea38af31ffda2e6b04586c9b352b3cb9d26ff
Eddy Kubwimana <eddy.kubwimana@ashesi.edu.gh>
2022-12-08 23:13:56 +0000

    I added the function for finding a remainder without using %

--- integer division without mod.py
+++ integer division without mod.py
@@ -34,7 +34,26 @@ def integerdiv (dividend, divisor):
              
         return -incr
 
-print(integerdiv(-10,10))
+#print(integerdiv(-10,10))
+
+
+#finding the remainder without using %
+def remainder(dividend,divisor):
+    if divisor>dividend:
+        return dividend
+    elif divisor == dividend:
+        return 0
+    else:
+        incr = divisor
+        while dividend > incr:
+            incr += divisor
+            if dividend == incr:
+                return 0
+        incr -=divisor
+        return dividend-incr
+print(remainder(135676,1237),135676%1237)
+        
+    
         
         
                 
